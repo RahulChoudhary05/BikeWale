abad3f706541fd3fec165f591d2a906bc614c309
Aditya Sharma <135639134+AdY21850@users.noreply.github.com>
2025-04-15 03:02:54 +0530

    Update Profile.js

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -195,7 +195,7 @@ exports.updateDisplayPicture = async (req, res) => {
       const userId = req.user.id;
 
       // TODO: Save file URL or path to the database for this user
-      const fileUrl = /uploads/${displayPicture.filename}; // Or your S3/cloud location
+      const fileUrl = '/uploads/' + displayPicture.filename; // Or your S3/cloud location
 
       console.log(User ${userId} uploaded profile picture: ${fileUrl});
 
