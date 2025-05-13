e434fdef353c3d793e2a52173d3ff8392cda15de
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-05-13 10:53:11 +0530

    ...

--- routes/Wallet.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createOrder, verifyPayment } = require("../controllers/wallet");
-
-const router = express.Router();
-
-router.post("/create-order", createOrder);
-router.post("/verify-payment", verifyPayment);
-
-module.exports = router;
